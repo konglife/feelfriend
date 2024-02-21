17aa30ecbab78907d032f78e1d6e6d3f37e2559f
KAINGKAI JEAMRA <kongfix.android@gmail.com>
2024-02-21 16:43:54 +0700

    Update index.js
    test first dev repo

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('สวัสดี Express');
+    res.send('สวัสดี Express dev');
 });
 
 app.listen(9753, () => {
